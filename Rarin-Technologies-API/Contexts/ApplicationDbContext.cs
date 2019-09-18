e81c4ce2cbddcad506d34a7351911c663388c154
ItsRicmor <ricardojms1999@gmail.com>
2019-09-17 21:06:56 -0600

    Create Category Model and DbContext

---
+++ Rarin-Technologies-API/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rarin_Technologies_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rarin_Technologies_API.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        DbSet<Category> Categories { get; set; }
+    }
+}
