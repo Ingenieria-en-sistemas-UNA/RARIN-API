3eb128ab948ac3b68fc69451164d08db2a93eb37
Arlen <arlenvargas2000@gmail.com>
2019-09-20 19:56:40 -0600

    Create entities categori and products

---
+++ Rarin-Technologies-API/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rarin_Technologies_API.Entities
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+    }
+}
---
+++ Rarin-Technologies-API/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rarin_Technologies_API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Category Category { get; set; }
+    }
+}
+    
